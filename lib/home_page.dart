a476d9b8b7776943a23817d30c7af8151cdc1963
Muhammad Shakir <81479962+mshakir01@users.noreply.github.com>
2023-02-13 16:26:54 +0500

    init commit
    Text, TextFormField, Button

---
+++ lib/home_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Fist App"),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "This Our First Project",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Login Form",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+
+            TextFormField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.email),
+                hintText: "Email",
+              ),
+            ),
+
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.lock),
+                  hintText: "Password"),
+            ),
+
+            SizedBox(
+              height: 50,
+            ),
+            // Container(
+            //   width: 300,
+            //   height: 100,
+            //   color: Colors.brown,
+            // ),
+            // Container(
+            //   color: Colors.amber,
+            //   width: 300,
+            //   height: 100,
+            // ),
+            // Container(
+            //   color: Colors.red,
+            //   width: 300,
+            //   height: 100,
+            // ),
+            // SizedBox(
+            //   height: 50,
+            // ),
+            // Row(
+            //   children: [
+            //     Container(
+            //       width: 100,
+            //       height: 100,
+            //       color: Colors.brown,
+            //     ),
+            //     Container(
+            //       color: Colors.amber,
+            //       width: 100,
+            //       height: 100,
+            //     ),
+            //     Container(
+            //       color: Colors.red,
+            //       width: 100,
+            //       height: 100,
+            //     ),
+            //   ],
+            // ),
+
+            ElevatedButton(onPressed: () {}, child: Text("Button"))
+          ],
+        ));
+  }
+}
